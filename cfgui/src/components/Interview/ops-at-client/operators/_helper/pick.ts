175a6e4e06cb5773fb507712c31afdff58f5fefb
Basab Dattaray <91115353+basab-dattaray@users.noreply.github.com>
2024-03-07 15:28:11 -0800

    cfgui: abstracting pickmany - START

--- cfgui/src/components/Interview/ops-at-client/operators/_helper/pick.ts
+++ cfgui/src/components/Interview/ops-at-client/operators/_helper/pick.ts
@@ -1,5 +1,8 @@
 import { JsonObjectType } from "../../../../../shared/defs/types";
-import { fnSetQueryAttribute } from "../../../state-mgt/dataAccess/loLevelAccess";
+import {
+  fnGetQueryObject,
+  fnSetQueryAttribute,
+} from "../../../state-mgt/dataAccess/loLevelAccess";
 
 export const fnProcessGrandChildren = (parentSid: string, children: object) => {
   for (const [key, value] of Object.entries(children as object)) {
@@ -21,4 +24,43 @@ export const fnProcessGrandChildren = (parentSid: string, children: object) => {
     }
   }
   return { error: null };
+};
+
+export const fnFindDescendantInfo = (sidCursor: string) => {
+  const { error, queryObject } = fnGetQueryObject(sidCursor);
+  if (error) {
+    return { error };
+  }
+  interface ObjTemplate {
+    children?: object;
+    sid?: string;
+    val?: number[];
+  }
+
+  const {
+    children,
+    sid: parentSid,
+    val: derivedIndices,
+  } = (queryObject || {}) as ObjTemplate;
+  if (!children) {
+    return { error: "No children found", descendantInfo: null };
+  }
+  if (typeof parentSid !== "string") {
+    return { error: "sid is not a string" };
+  }
+  if (!Array.isArray(derivedIndices)) {
+    return { error: "improper indices", descendantInfo: null };
+  }
+
+  interface ObjTemplateChildren {
+    kind?: string;
+    val?: string[] | number[] | boolean[] | undefined;
+  }
+  const { kind: childrenKind, val: childrenVal } =
+    children as ObjTemplateChildren;
+
+  return {
+    error: null,
+    descendantInfo: { parentSid, derivedIndices, childrenKind, childrenVal },
+  };
 };
