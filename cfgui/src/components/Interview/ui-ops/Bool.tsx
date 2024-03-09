e37998d78605e5f6fe3e8627ed2d5d8bf1334907
Basab Dattaray <91115353+basab-dattaray@users.noreply.github.com>
2024-03-09 01:13:35 -0800

    cfgui: adding error message to Bool.tsx: DONE

--- cfgui/src/components/Interview/ui-ops/Bool.tsx
+++ cfgui/src/components/Interview/ui-ops/Bool.tsx
@@ -1,5 +1,4 @@
 import React, { useState, useEffect, useCallback } from "react";
-
 import { KEY_VAL } from "../../../shared/defs/constants";
 import { fnSetQueryAttribute } from "../state-mgt/dataAccess/loLevelAccess";
 import { JsonObjectType } from "../../../shared/defs/types";
@@ -63,28 +62,28 @@ const Bool: React.FC<Props> = ({ queryObject, onResponse }) => {
 
   return (
     <div className='flex items-center'>
-      <div className='flex flex-col'>
-        <label>
-          <input
-            type='radio'
-            name='boolRadio'
-            value='true'
-            checked={value === true}
-            onChange={() => handleInputChange(true)}
-          />
-          True
-        </label>
-        <label>
-          <input
-            type='radio'
-            name='boolRadio'
-            value='false'
-            checked={value === false}
-            onChange={() => handleInputChange(false)}
-          />
-          False
-        </label>
-      </div>
+      <label className='inline-flex items-center mr-2'>
+        <input
+          type='radio'
+          name='boolRadio'
+          value='true'
+          checked={value === true}
+          onChange={() => handleInputChange(true)}
+          className='form-radio h-5 w-5 text-blue-500'
+        />
+        <span className='ml-2'>True</span>
+      </label>
+      <label className='inline-flex items-center'>
+        <input
+          type='radio'
+          name='boolRadio'
+          value='false'
+          checked={value === false}
+          onChange={() => handleInputChange(false)}
+          className='form-radio h-5 w-5 text-blue-500'
+        />
+        <span className='ml-2'>False</span>
+      </label>
       <div className='flex-grow' />
       <button
         className={`bg-blue-500 text-white px-4 py-2 rounded-md ${
