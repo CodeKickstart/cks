d26f174441c1e85757ed27b8f14b1d7cc7103771
Basab Dattaray <91115353+basab-dattaray@users.noreply.github.com>
2024-03-13 02:27:48 -0700

    cfgsrv: added validateEntireTree: DONE

--- cfgsrv/src/context/validation/validateEntireTree.ts
+++ cfgsrv/src/context/validation/validateEntireTree.ts
@@ -1,7 +1,20 @@
 import {
+  KEY_BLOCKED,
   KEY_CHILDREN,
+  KEY_COMMENTS,
   KEY_CONTENT,
+  KEY_DEFVAL,
+  KEY_DPICK,
+  KEY_ID,
+  KEY_INFO,
   KEY_KIND,
+  KEY_MAX,
+  KEY_MIN,
+  KEY_OVERRIDE,
+  KEY_PARENT_UID,
+  KEY_PROMPT,
+  KEY_SID,
+  KEY_VAL,
 } from "../../shared/defs/constants";
 import { JsonObjectType } from "../../shared/defs/types";
 import { Str } from "../../typeStr";
@@ -9,38 +22,59 @@ import { Str } from "../../typeStr";
 export const fnValidateTreeNodesWithPreTraversal = (
   jsonObj: JsonObjectType
 ): { error: Str } => {
+  const _fnIsKeyCustom = (key: string): boolean => {
+    const regularKeys = [
+      KEY_CHILDREN,
+      KEY_CONTENT,
+      KEY_KIND,
+      KEY_DEFVAL,
+      KEY_ID,
+      KEY_SID,
+      KEY_BLOCKED,
+      KEY_COMMENTS,
+      KEY_DPICK,
+      KEY_INFO,
+      KEY_PARENT_UID,
+      KEY_PROMPT,
+      KEY_MAX,
+      KEY_MIN,
+      KEY_VAL,
+      KEY_OVERRIDE,
+    ];
+    if (regularKeys.includes(key)) {
+      return false;
+    }
+    return true;
+  };
   function _fnValidateTree(
+    key: string | null,
     parentKey: string | null,
-    parentObj: JsonObjectType
+    obj: JsonObjectType
   ): { error: Str } {
-    // if (parentKey === KEY_CHILDREN && !Array.isArray(parentObj)) {
-    //   return { error: `Children must be an array` };
-    // }
-    const allKeys = Object.keys(parentObj);
+    if (
+      key !== null &&
+      _fnIsKeyCustom(key) &&
+      parentKey !== null &&
+      _fnIsKeyCustom(parentKey)
+    ) {
+      return {
+        error: `Cannot have a custom node (${key}) under another custom node (${parentKey})`,
+      };
+    }
+    const allKeys = Object.keys(obj);
     if (allKeys.includes(KEY_CHILDREN) && !allKeys.includes(KEY_KIND)) {
       return {
         error: `Cannot have a "children" key without a key named "kind"`,
       };
     }
-    for (const [keyChild, childObj] of Object.entries(parentObj)) {
-      // if (
-      //   parentKey !== null &&
-      //   parentKey !== KEY_CHILDREN &&
-      //   key !== KEY_CHILDREN &&
-      //   parentKey !== KEY_CONTENT &&
-      //   key !== KEY_CONTENT
-      // ) {
-      //   return {
-      //     error: `Cannot have a "non-children" node under a "non-children" node`,
-      //   };
-      // }
+    for (const [keyChild, childObj] of Object.entries(obj)) {
       if (typeof childObj === "object" && childObj !== null) {
-        _fnValidateTree(keyChild, childObj as JsonObjectType);
+        return _fnValidateTree(keyChild, key, childObj as JsonObjectType);
       }
     }
     return { error: null };
   }
 
-  const { error } = _fnValidateTree(null, jsonObj);
+  const { error } = _fnValidateTree(null, null, jsonObj);
   return { error };
 };
