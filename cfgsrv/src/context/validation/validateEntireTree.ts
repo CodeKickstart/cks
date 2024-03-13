6b81f4ab57db15039a4b28aff92e991c865b9752
Basab Dattaray <91115353+basab-dattaray@users.noreply.github.com>
2024-03-13 01:40:47 -0700

    cfgsrv: adding validateTree - WIP 3

--- cfgsrv/src/context/validation/validateEntireTree.ts
+++ cfgsrv/src/context/validation/validateEntireTree.ts
@@ -13,30 +13,29 @@ export const fnValidateTreeNodesWithPreTraversal = (
     parentKey: string | null,
     parentObj: JsonObjectType
   ): { error: Str } {
-    if (parentKey === KEY_CHILDREN && !Array.isArray(parentObj)) {
-      return { error: `Children must be an array` };
-    }
+    // if (parentKey === KEY_CHILDREN && !Array.isArray(parentObj)) {
+    //   return { error: `Children must be an array` };
+    // }
     const allKeys = Object.keys(parentObj);
     if (allKeys.includes(KEY_CHILDREN) && !allKeys.includes(KEY_KIND)) {
       return {
         error: `Cannot have a "children" key without a key named "kind"`,
       };
     }
-    for (const [key, childObj] of Object.entries(parentObj)) {
+    for (const [keyChild, childObj] of Object.entries(parentObj)) {
+      // if (
+      //   parentKey !== null &&
+      //   parentKey !== KEY_CHILDREN &&
+      //   key !== KEY_CHILDREN &&
+      //   parentKey !== KEY_CONTENT &&
+      //   key !== KEY_CONTENT
+      // ) {
+      //   return {
+      //     error: `Cannot have a "non-children" node under a "non-children" node`,
+      //   };
+      // }
       if (typeof childObj === "object" && childObj !== null) {
-        // Recursively validate nested objects
-        if (
-          parentKey != null &&
-          parentKey !== KEY_CHILDREN &&
-          key !== KEY_CHILDREN &&
-          key != KEY_CONTENT
-        ) {
-          return {
-            error: `Cannot have a "non-children" node under a "non-children" node`,
-          };
-        }
-
-        _fnValidateTree(key, childObj as JsonObjectType);
+        _fnValidateTree(keyChild, childObj as JsonObjectType);
       }
     }
     return { error: null };
