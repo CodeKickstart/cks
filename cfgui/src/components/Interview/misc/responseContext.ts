d02ced69d4db4dea5d12a89c1b96e445ba26a1ab
Basab Dattaray <91115353+basab-dattaray@users.noreply.github.com>
2024-03-14 04:11:55 -0700

    cfgui: ResponseContext - WIP 2

--- cfgui/src/components/Interview/misc/responseContext.ts
+++ cfgui/src/components/Interview/misc/responseContext.ts
@@ -1,15 +1,39 @@
+import { Str } from "../defs/types/Str";
 import { valtioStore } from "../defs/types/ValtioTypes";
 
-export const fnGetResponseContext = () => {
-  // return valtioStore.postOrderList;
+export const fnGetResponseContext = (): {
+  error: Str;
+  responseContext: { [key: string]: unknown };
+} => {
+  let error: Str = null;
+
   const responseContext = {} as { [key: string]: unknown };
 
   const queryContext = valtioStore.queryContext as { [key: string]: unknown };
-  for (const key in queryContext) {
-    const value = queryContext[key];
+  for (const queryName in queryContext) {
+    const queryValue = queryContext[queryName];
+    interface ObjTemplate {
+      processedQuery?: { [key: string]: unknown };
+    }
 
-    responseContext[key] = value;
+    const { processedQuery } = queryValue as ObjTemplate;
+    if (processedQuery) {
+      interface ObjTemplateProcessedQuery {
+        sid?: string;
+        val?: unknown;
+      }
+      const { sid, val } = processedQuery as ObjTemplateProcessedQuery;
+
+      if (sid === undefined) {
+        error = `sid or val is undefined for queryName: ${queryName}`;
+        return { error, responseContext: {} };
+      }
+
+      if (!sid.startsWith("zzz")) {
+        responseContext[sid] = val;
+      }
+    }
   }
 
-  return responseContext;
+  return { error, responseContext };
 };
