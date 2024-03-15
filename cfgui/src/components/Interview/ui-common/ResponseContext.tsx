c0f676269ec2e9d4b5b2b06cda0c0c571520eb30
Basab Dattaray <91115353+basab-dattaray@users.noreply.github.com>
2024-03-15 15:30:30 -0700

    cfgui: streamlining ResponseContext.tsx DONE

--- cfgui/src/components/Interview/ui-common/ResponseContext.tsx
+++ cfgui/src/components/Interview/ui-common/ResponseContext.tsx
@@ -28,7 +28,7 @@ const ResponseContext: React.FC = () => {
         <h1 className='text-xl font-bold mb-2'>Responses</h1>
         <div className='overflow-y-auto h-full'>
           <pre className='whitespace-pre-wrap'>
-            <ReactJson src={responseContextVal as object} />
+            <pre>{JSON.stringify(responseContextVal as object, null, 2)}</pre>
           </pre>
         </div>
       </div>
