d2d4179ee27fec06ca65b87fbb7e24acdb06bb1c
Basab Dattaray <91115353+basab-dattaray@users.noreply.github.com>
2024-04-08 16:06:01 -0700

    gitignore adds

--- cfgui/src/components/Interview/ui-common/QuestionResponses.tsx
+++ cfgui/src/components/Interview/ui-common/QuestionResponses.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-// import { logListingSuccess } from "../state-mgt/setupForResponse";
+
 import { fnGetAllPreOrderAnswers } from "../state-mgt/dataAccess/hiLevelAccess";
 import { KEY_VAL } from "../../../shared/defs/constants";
 
@@ -12,20 +12,9 @@ const QuestionResponses: React.FC = () => {
     return <div className='text-red-500'>Answers not found</div>;
   }
 
-  // const { error: errorNameLists, results: childNameLists } =
-  //   fnGetAllPreOrderAnswers<string[]>("descendantNames");
-  // if (errorNameLists) {
-  //   return <div className='text-red-500'>{errorNameLists}</div>;
-  // }
-  // if (childNameLists === null) {
-  //   return <div className='text-red-500'>Name lists not found</div>;
-  // }
-
   return (
     <div className='mb-4'>
-      <div className='flex'>
-        <h3 className='text-lg font-semibold mb-2'>Questions answered!</h3>
-      </div>
+      <h3 className='text-lg font-semibold mb-2'>Questions answered!</h3>
       <ul>
         {valList.map((answer, index) => (
           <li key={index}>{`Question ${index + 1}: ${answer}`}</li>
