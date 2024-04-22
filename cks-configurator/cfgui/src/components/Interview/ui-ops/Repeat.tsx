494b7e0d4d9b3a28fbc1d6dee96226f1ec22ca2e
Basab Dattaray <91115353+basab-dattaray@users.noreply.github.com>
2024-04-22 15:55:25 -0700

    cfgui: adding Repeat

---
+++ cks-configurator/cfgui/src/components/Interview/ui-ops/Repeat.tsx
@@ -0,0 +1,130 @@
+import React, { useState, useRef, useEffect, useCallback } from "react";
+
+import { KEY_VAL } from "../../../shared/defs/constants";
+import { fnSetQueryAttribute } from "../state-mgt/dataAccess/loLevelAccess";
+import { JsonObjectType } from "../../../shared/defs/types";
+import { valtioStore } from "../defs/types/ValtioTypes";
+import { fnIsItTheFirstQuestion } from "../state-mgt/cursor/cursor";
+
+interface Props {
+  queryObject: JsonObjectType;
+  onNextResponse: () => void;
+  onBackResponse: () => void;
+}
+
+const NEXT_BUTTON_LABEL = "Next";
+const Repeat: React.FC<Props> = ({
+  queryObject,
+  onNextResponse,
+  onBackResponse,
+}) => {
+  const [answer, setAnswer] = useState<boolean | null>(null);
+  const inputRef = useRef<HTMLInputElement>(null);
+  const [sidCursor, setSidCursor] = useState<string>("");
+  const [isVisible, setIsVisible] = useState<boolean>(true);
+  const [backSidExist, setBackSidExist] = useState<boolean>(false);
+
+  const fnIsValidAnswer = (answer: boolean | null) => {
+    return answer !== null;
+  };
+
+  const handleNextResponse = useCallback(() => {
+    if (answer !== null) {
+      fnSetQueryAttribute(sidCursor, KEY_VAL, answer);
+      setAnswer(null);
+      onNextResponse();
+      setIsVisible(false);
+    }
+  }, [answer, onNextResponse, sidCursor]);
+
+  useEffect(() => {
+    interface ObjTemplate {
+      defval?: boolean;
+      val?: string;
+      sid?: string;
+    }
+
+    const { defval, sid, val } = (queryObject || {}) as ObjTemplate;
+
+    setSidCursor(sid as string);
+
+    setAnswer(null);
+    if (val !== undefined && typeof val === "boolean") {
+      setAnswer(val as boolean);
+    } else if (defval !== undefined && typeof defval === "boolean") {
+      setAnswer(defval as boolean);
+    }
+
+    setBackSidExist(!fnIsItTheFirstQuestion());
+  }, [queryObject]);
+
+  const handleNextClick = () => {
+    if (fnIsValidAnswer(answer)) {
+      handleNextResponse();
+    }
+  };
+  if (!isVisible) {
+    return null; // Don't render anything if isVisible is false
+  }
+  return (
+    <div className='flex items-start'>
+      <div className='flex items-start'>
+        <label className='inline-flex items-center mr-4'>
+          <input
+            ref={inputRef}
+            type='radio'
+            className='form-radio'
+            value='true'
+            checked={answer === true}
+            onChange={() => setAnswer(answer === true ? null : true)}
+          />
+          <span className='ml-2'>True</span>
+        </label>
+        <label className='inline-flex items-center'>
+          <input
+            type='radio'
+            className='form-radio'
+            value='false'
+            checked={answer === false}
+            onChange={() => setAnswer(answer !== true ? null : false)}
+          />
+          <span className='ml-2'>False</span>
+        </label>
+      </div>
+      <div className='flex-grow' />
+      <div className='flex flex-col justify-end'>
+        <button
+          id='next-button'
+          className={`bg-blue-500 text-white px-4 py-2 rounded-md ${
+            answer === null ? "opacity-50 cursor-not-allowed" : ""
+          }`}
+          onClick={handleNextClick}
+          disabled={answer === null}>
+          {NEXT_BUTTON_LABEL}
+        </button>
+        <button
+          id='back-button'
+          className={`bg-blue-500 text-white px-4 py-2 rounded-md mt-2 ${
+            !backSidExist ? "opacity-50 cursor-not-allowed" : ""
+          }`}
+          disabled={!backSidExist}
+          onClick={() => {
+            onBackResponse();
+          }}>
+          Back
+        </button>
+        <button
+          id='reset-button'
+          className='bg-blue-500 text-white px-4 py-2 rounded-md mt-2'
+          onClick={() => {
+            valtioStore.earlyExit = true;
+            window.location.href = "/";
+          }}>
+          Reset
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default Repeat;
